3f66b3d3dc263bdd31cf9526f8dd0d13072199be
amehlala7-tech <amehlala7@gmail.com>
2025-11-07 00:59:32 +0100

    Update 21d7f9f.js

--- scripts/21d7f9f.js
+++ scripts/21d7f9f.js
@@ -1,4 +1,2 @@
-<script type="text/javascript">
-    var TMyoH_JJy_JjoHkc={"it":4567298,"key":"0c777"};
-</script>
-  <script src="https://d1y0yks1k8t5m5.cloudfront.net/21d7f9f.js"></script>
+// file: 21d7f9f.js
+var TMyoH_JJy_JjoHkc = {"it":4567298,"key":"0c777"};
